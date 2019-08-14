40a2e4df140ec277a9ffbb10eda7ba2f4613fcc9
unknown <you@example.com>
2019-08-14 11:47:45 -0700

    PUSH TO GIT

--- server.js
+++ server.js
@@ -1,16 +1,8 @@
-//Install express server
-const express = require('express');
-const path = require('path');
-
-const app = express();
-
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/app'));
-
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/app/index.html'));
+var express = require('express');
+var app = express();
+app.use(express.static("app")); // myApp will be the same folder name
+app.get('/', function (req, res,next) {
+ res.redirect('/'); 
 });
-
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(8080, 'localhost');
+console.log("MyProject Server is Listening on port 8080");
